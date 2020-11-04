abb92c5e0dc08e43afc7689eb9b8f93491dd33af
Cristopher <crispc@Cristophers-MacBook-Pro.local>
2020-11-04 16:09:19 -0400

    Update contract addresses

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -80,5 +80,5 @@ export const WETH = {
   ),
   [ChainId.GÖRLI]: new Token(ChainId.GÖRLI, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 18, 'WETH', 'Wrapped Ether'),
   [ChainId.KOVAN]: new Token(ChainId.KOVAN, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH', 'Wrapped Ether'),
-  [ChainId.IDCHAIN]: new Token(ChainId.IDCHAIN, '0x2b309226500ADc5956a422950A2AD6E6333Bb315', 18, 'WEIDI', 'Wrapped Eidi')
+  [ChainId.IDCHAIN]: new Token(ChainId.IDCHAIN, '0xDA3F5196974e6F044Bc95276e68830b235Ef25fE', 18, 'WEIDI', 'Wrapped Eidi')
 }
